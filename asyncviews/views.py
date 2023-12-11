2f0242f1e594dd8d21c4707afb1fa6fbf0888a0c
IsacJfs <isac.jfs@gmail.com>
2023-12-11 09:01:38 -0300

    :bug: [FIX] corrigindo views.py

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -1,5 +1,6 @@
 import asyncio
 import httpx
+from django.http import HttpResponse
 
 async def http_call_async():
     for num in range(1, 6):
@@ -8,3 +9,7 @@ async def http_call_async():
     async with httpx.AsyncClient() as client:
         response = await client.get('https://httpbin.org/')
         print(response)
+
+async def async_view(request):
+    await http_call_async()
+    return HttpResponse('Non-blocking response')
